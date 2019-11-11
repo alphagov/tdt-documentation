e50089057c014be8d91208bea927802fd687e47a
Andrea Szöllössi <andrea.szollossi@digital.cabinet-office.gov.uk>
2019-11-11 16:11:42 +0000

    Remove gem_docs helper
    Now that we have all content in one place (previous commit), we don't 
    need the gem_docs helper to bring the content in from the gem repo.

--- config.rb
+++ config.rb
@@ -16,19 +16,3 @@ after_build do |builder|
     abort e.to_s
   end
 end
-
-
-DOCS_LOCATION_IN_GEM = Bundler.rubygems.find_name('govuk_tech_docs').first.full_gem_path + "/docs"
-
-files.watch :source, path: DOCS_LOCATION_IN_GEM
-
-helpers do
-  def gem_docs(filename)
-    raw_markdown = File.read(DOCS_LOCATION_IN_GEM + "/#{filename}")
-
-    # Strip the h1 header from the original markdown file
-    markdown = raw_markdown.lines[1..-1].join
-
-    markdown
-  end
-end
