8d7ce7495babdd2b2654e260d5961f2625f21db7
Andrea Szöllössi <andrea.szollossi@digital.cabinet-office.gov.uk>
2019-11-11 15:39:59 +0000

    Set up html-proofer as the link checker
    Following configuration from
    https://github.com/alphagov/tech-docs-template/pull/187

--- config.rb
+++ config.rb
@@ -1,6 +1,22 @@
 require 'govuk_tech_docs'
 
+# Check for broken links
+require 'html-proofer'
+
 GovukTechDocs.configure(self, livereload: { js_host: "localhost" })
+
+after_build do |builder|
+  begin
+    HTMLProofer.check_directory(config[:build_dir],
+      { :assume_extension => true,
+        :disable_external => true,
+        :allow_hash_href => true,
+        :url_swap => { config[:tech_docs][:host] => "" } }).run
+  rescue RuntimeError => e
+    abort e.to_s
+  end
+end
+
 
 DOCS_LOCATION_IN_GEM = Bundler.rubygems.find_name('govuk_tech_docs').first.full_gem_path + "/docs"
 
