049306c47f551e5e96f31dbe3f152df715e30841
tobias.saunders <tobias.saunders@digital.cabinet-office.gov.uk>
2024-06-26 15:12:52 +0100

    Updated preview script with correct tag name

--- preview-with-docker.sh
+++ preview-with-docker.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 set -e
 
-docker build . --tag di-team-manual
+docker build . --tag tdt-documentation
 
-docker run --rm -p 4567:4567 -p 35729:35729 -v $(pwd):/usr/src/docs -it di-team-manual
+docker run --rm -p 4567:4567 -p 35729:35729 -v $(pwd):/usr/src/docs -it tdt-documentation
